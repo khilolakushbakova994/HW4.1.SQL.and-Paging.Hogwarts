9a967ca49a9523b442096e2fa3634cc7712dfe49
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-02-02 15:46:51 +0100

    Initial commit

---
+++ src/main/java/hogwarts/HW41/SQL/and/Paging/Hogwarts/Application.java
@@ -0,0 +1,13 @@
+package hogwarts.HW41.SQL.and.Paging.Hogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
