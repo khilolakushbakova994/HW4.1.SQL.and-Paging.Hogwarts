bf74ccd62dfa88126c5592a53fcc40a4d4ce3432
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-02-02 16:04:15 +0100

    hw is done

--- src/main/java/hogwarts/HW41/SQL/and/Paging/Hogwarts/Application.java
+++ src/main/java/hogwarts/HW41/SQL/and/Paging/Hogwarts/Application.java
@@ -1,9 +1,11 @@
 package hogwarts.HW41.SQL.and.Paging.Hogwarts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/hogwarts/HW41/SQL/and/Paging/Hogwarts/repository/FacultyRepository.java
@@ -0,0 +1,23 @@
+package hogwarts.HW41.SQL.and.Paging.Hogwarts.repository;
+
+
+
+
+import hogwarts.HW41.SQL.and.Paging.Hogwarts.model.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+
+@Repository
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+    Collection<Faculty> findFacultyByColorOrNameContainsIgnoreCase  (String color, String name);
+    Collection<Faculty>findFacultyByNameIgnoreCase (String name);
+    Collection<Faculty>findFacultyByColorIgnoreCase (String color);
+
+
+
+
+}
+
